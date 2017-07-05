f0904c915b56da987c29f8d9473f2a2cb7347e17
lingzhi <1534649018@qq.com>
2017-07-05 10:22:01 +0800

    messageController添加tableView

--- M-plus/ipad/LZModule/message/LZMessageViewControllerPad.h
+++ M-plus/ipad/LZModule/message/LZMessageViewControllerPad.h
@@ -9,5 +9,7 @@
 #import <UIKit/UIKit.h>
 
 @interface LZMessageViewControllerPad : UIViewController
+
+
 - (void)levelOneViewWillAppear;
 @end
--- M-plus/ipad/LZModule/message/LZMessageViewControllerPad.m
+++ M-plus/ipad/LZModule/message/LZMessageViewControllerPad.m
@@ -7,13 +7,33 @@
 //
 
 #import "LZMessageViewControllerPad.h"
+#define ModuleTableWidth 63.0
+#define LevelOneVCWidth  350.0
+#define LevelTwoVCWidth  (1024 - ModuleTableWidth - LevelOneVCWidth)
+#define LevelTwoNaviBarH 65.0
+@interface LZMessageViewControllerPad ()<UITableViewDelegate,UITableViewDataSource>
 
-@interface LZMessageViewControllerPad ()
+@property (nonatomic,strong)UITableView *messageTableView;
 
 @end
 
 @implementation LZMessageViewControllerPad
 
+
+//懒加载
+- (UITableView *)messageTableView
+{
+    if (_messageTableView == nil) {
+        _messageTableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, LevelOneVCWidth, 748) style:UITableViewStylePlain];
+        
+        _messageTableView.showsHorizontalScrollIndicator = NO;
+        _messageTableView.showsVerticalScrollIndicator = NO;
+        _messageTableView.delegate = self;
+        _messageTableView.dataSource = self;
+        
+    }
+    return _messageTableView;
+}
 
 - (void)levelOneViewWillAppear
 {
@@ -22,7 +42,34 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self.view setBackgroundColor:[UIColor redColor]];
+//    [self.view setBackgroundColor:[UIColor redColor]];
+    [self.view addSubview:self.messageTableView];
+}
+
+#pragma mark- 
+#pragma mark- UITableViewDelegate,UITableViewDataSource
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 50;
+    
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"CELL"];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"CELL"];
+        cell.textLabel.text = [NSString stringWithFormat:@"%ld",(long)indexPath.row];
+    }
+    return cell;
+    
+    
+    
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 44.0;
 }
 
 - (void)didReceiveMemoryWarning {
